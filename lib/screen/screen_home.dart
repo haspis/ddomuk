33128838ce496f9ff26c291d11a48bd2e46ba370
haspi <hsp_s@naver.com>
2022-09-06 12:45:23 +0900

    basic structure

---
+++ lib/screen/screen_home.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('또먹안먹'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/title.png',
+                width: width * 0.8,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              '또먹안먹',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '먹을래말래',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1'),
+            _buildStep(width, '2'),
+            _buildStep(width, '3'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ElevatedButton(
+                    child: Text(
+                      'NOW',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    //style: backgroundColor: Colors.deepPurple,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
